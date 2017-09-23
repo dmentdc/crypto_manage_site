94e6274c0275081e9270dcaf8924d6f0ec925d0f
Develop <develop@MBPdesebastian.iptv.microsoft.com>
2017-09-23 13:48:19 -0300

    Creación modelo datos de app

--- crypto_manage_site/settings.py
+++ crypto_manage_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'manage_cryptomkt.apps.ManageCryptomktConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- manage_cryptomkt/models.py
+++ manage_cryptomkt/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
+
+class Coin(models.Model):
+    """data para Coin"""
+    nombre = models.CharField(max_length=200)
+
+class Precio(models.Model):
+    """Precio en un instante de una crypto moneda en un mercado"""
+    coin = models.ForeignKey(Coin, on_delete=models.DO_NOTHING, null=True)
+    valor = models.DecimalField(max_digits=12, decimal_places=2)
+    tipo = models.CharField(max_length=10, null=True)
+    moneda = models.CharField(max_length=5, null=True)
+    mercado = models.CharField(max_length=200, null=True)
