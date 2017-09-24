39bfb402dde8c70bf6f88b7e165c33a7b0f5502b
Develop <develop@MBPdesebastian.iptv.microsoft.com>
2017-09-23 22:50:53 -0300

    cambios

--- manage_cryptomkt/models.py
+++ manage_cryptomkt/models.py
@@ -1,11 +1,16 @@
+import datetime
+
 from django.db import models
 from decimal import Decimal
+from django.utils import timezone
 
 # Create your models here.
 
 class Coin(models.Model):
     """data para Coin"""
     nombre = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre
 
 class Precio(models.Model):
     """Precio en un instante de una crypto moneda en un mercado"""
@@ -14,3 +19,6 @@ class Precio(models.Model):
     tipo = models.CharField(max_length=10, null=True)
     moneda = models.CharField(max_length=5, null=True)
     mercado = models.CharField(max_length=200, null=True)
+    fecha = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return self.coin + self.valor
